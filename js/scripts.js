2a955b414842a81012b1016e5ed384424fdb67d9
Erica Nafziger and Luke Philips <luke@>
2016-10-24 10:17:48 -0700

    add looping in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var nounInput = $("input#noun").val();
-    var adjectiveInput = $("input#adjective").val();
-    var verbInput = $("input#verb").val();
-    var person2Input = $("input#person2").val();
 
-    $(".person1").text(person1Input);
-    $(".noun").text(nounInput);
-    $(".adjective").text(adjectiveInput);
-    $(".verb").text(verbInput);
-    $(".person2").text(person2Input);
+    var blanks = ["person1", "noun", "adjective", "verb", "person2"];
 
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+  
     $("#story").show();
     event.preventDefault();
 
